d25cb6785f11083997bb6ca72e3efbc6c696e279
Linn Johansen <linn.es.johansen@gmail.com>
2024-11-12 16:32:27 -0800

    Refactoring

---
+++ src/Stats.js
@@ -0,0 +1,18 @@
+export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className='stats'>
+        <em>Start adding some items to your packing list 📃👜</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
+  return (
+    <footer className='stats'>
+      <em>{percentage === 100 ? `Packing complete! You're ready to go!` : `You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}</em>
+    </footer>
+  );
+}
